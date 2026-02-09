8544317938afc50a28e81ab7bba75b198fe6ed55
buckman-maxwell <Max.r.buckman@gmail.com>
2026-02-09 18:25:03 -0500

    WorkingBot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,18 +33,18 @@ public final class Constants {
     public static final int INTAKE_LAUNCHER_MOTOR_ID = 5;
 
     // Current limit and nominal voltage for fuel mechanism motors.
-    public static final int FEEDER_MOTOR_CURRENT_LIMIT = 75;
-    public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 75;
+    public static final int FEEDER_MOTOR_CURRENT_LIMIT = 60;
+    public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 60;
 
     // Voltage values for various fuel operations. These values may need to be tuned
     // based on exact robot construction.
     // See the Software Guide for tuning information
-    public static final double INTAKING_FEEDER_VOLTAGE = 11.5;
-    public static final double INTAKING_INTAKE_VOLTAGE = -11.5;
-    public static final double LAUNCHING_FEEDER_VOLTAGE = -11.5;
-    public static final double LAUNCHING_LAUNCHER_VOLTAGE = -11.5;
+    public static final double INTAKING_FEEDER_VOLTAGE = 12;
+    public static final double INTAKING_INTAKE_VOLTAGE = -6;
+    public static final double LAUNCHING_FEEDER_VOLTAGE = -10;
+    public static final double LAUNCHING_LAUNCHER_VOLTAGE = -10;
     public static final double SPIN_UP_FEEDER_VOLTAGE = 10;
-    public static final double SPIN_UP_SECONDS = 2;
+    public static final double SPIN_UP_SECONDS = 1.75;
   }
 
   public static final class OperatorConstants {
