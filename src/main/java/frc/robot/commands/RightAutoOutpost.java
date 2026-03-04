a32c582ede99347ccc7d3149501f1d2b6757c300
adamyapathak <adamyapathak12@gmail.com>
2026-03-04 17:40:15 -0500

    Commiting again cause im not sure if it worked the 1st time.

--- src/main/java/frc/robot/commands/RightAutoOutpost.java
+++ src/main/java/frc/robot/commands/RightAutoOutpost.java
@@ -1,8 +1,5 @@
 package frc.robot.commands;
 
-import static frc.robot.Constants.AutoConstants.BASE_TIME;
-
-
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.CANClimbSubystem;
 import frc.robot.subsystems.CANDriveSubsystem;
@@ -11,13 +8,28 @@ import frc.robot.subsystems.CANFuelSubsystem;
 public class RightAutoOutpost extends SequentialCommandGroup{
 
     public RightAutoOutpost(CANDriveSubsystem driveSubsystem, CANFuelSubsystem fuelSubsystem, CANClimbSubystem climbSubsystem){
-        addCommands(
-            new AutoDrive(driveSubsystem,0,0.675).withTimeout(BASE_TIME* 1),
-            new AutoDrive(driveSubsystem,-1,0).withTimeout(BASE_TIME* 0.8),
-            new AutoDrive(driveSubsystem,0,0).withTimeout(BASE_TIME* 3),
-            new AutoDrive(driveSubsystem,1,0).withTimeout(BASE_TIME* 0.8),
-            new AutoDrive(driveSubsystem,0,0.675).withTimeout(BASE_TIME* 0.5),
-            new LaunchSequence(fuelSubsystem).withTimeout(BASE_TIME* 8));
-    }
-            };
 
+        addCommands(
+            //NOTE 0.1 AutoDrives are for testing until brake mode feature is implemented for drifting. This may or may not be solved by comp?
+            new AutoDrive(driveSubsystem, -1, 0).withTimeout(0.8),
+            new AutoDrive(driveSubsystem, -0.1, 0).withTimeout(0.5),
+            //Drives backwards for outpost for 0.8 seconds
+            new AutoDrive(driveSubsystem, 1, 0).withTimeout(0.8),
+            new AutoDrive(driveSubsystem, 0.1, 0).withTimeout(0.5),
+            //Drives foward for 0.8 seconds - original location -
+            new AutoDrive(driveSubsystem, 0, -0.675).withTimeout(0.5),
+            new AutoDrive(driveSubsystem, 0, -0.1).withTimeout(0.5),
+            //Turns counterclockwise-left for 0.5 second
+            new AutoDrive(driveSubsystem, 1, 0).withTimeout(2),
+            new AutoDrive(driveSubsystem, 0.1, 0).withTimeout(0.5),
+            //Drive foward for 2 seconds towards hub
+            new AutoDrive(driveSubsystem, 0, 0.675).withTimeout(0.5),
+            new AutoDrive(driveSubsystem, 0, 0.1).withTimeout(0.5),
+            //Turn clockwise-right for 0.5 a second
+            new AutoDrive(driveSubsystem, -1, 0).withTimeout(0.25),
+            new AutoDrive(driveSubsystem, -0.1, 0).withTimeout(0.5),
+            //Drives backward for 0.25 seconds so balls dont over shoot?
+            new LaunchSequence(fuelSubsystem).withTimeout(5));
+            //shoots loaded balls into hub for 5 seconds
+    }
+    }
